a89ccbb3513f36eb64a17a0667e56ecbfb604d65
Cole Pedersen <colep@lendio.com>
2021-01-27 14:25:41 -0700

    scaffold out pages

---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <p>Hey! I'm on the order page sucka</p>
+    </div>
+  );
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SlicemastersPage() {
+  return (
+    <div>
+      <p>Hey! I'm on the slice masters page sucka</p>
+    </div>
+  );
+}
