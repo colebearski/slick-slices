ab3485f689ae0e8c2debae699ef73a9c94d6c714
Cole Pedersen <colep@lendio.com>
2021-01-27 20:58:20 -0700

    nav setup, probs refactor into a layout

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function PizzasPage() {
   return (
     <div>
+      <Nav />
       <p>Hey! I'm on the pizzas page sucka</p>
     </div>
   );
--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function SlicemastersPage() {
   return (
     <div>
+      <Nav />
       <p>Hey! I'm on the slice masters page sucka</p>
     </div>
   );
